09ae6fca8b1277313c5419051359b12b282310b6
Henrik Aasted Sørensen <has@bitcraft.dk>
2019-08-06 08:55:50 +0200

    Add requirement to the supply keeper

--- x/mint/internal/types/expected_keepers.go
+++ x/mint/internal/types/expected_keepers.go
@@ -2,6 +2,7 @@ package types // noalias
 
 import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/x/supply"
 	"github.com/cosmos/cosmos-sdk/x/supply/exported"
 )
 
@@ -21,4 +22,5 @@ type SupplyKeeper interface {
 	SendCoinsFromModuleToAccount(ctx sdk.Context, senderModule string, recipientAddr sdk.AccAddress, amt sdk.Coins) sdk.Error
 	SendCoinsFromModuleToModule(ctx sdk.Context, senderModule, recipientModule string, amt sdk.Coins) sdk.Error
 	MintCoins(ctx sdk.Context, name string, amt sdk.Coins) sdk.Error
+	GetSupply(ctx sdk.Context) (supply supply.Supply)
 }
