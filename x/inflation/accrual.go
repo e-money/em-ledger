142d70a6a4642583f18630419710fe6af43f1d64
Henrik Aasted Sørensen <has@bitcraft.dk>
2019-09-11 09:46:06 +0200

    Allow inflation to start in the future

--- x/inflation/accrual.go
+++ x/inflation/accrual.go
@@ -62,8 +62,14 @@ func BeginBlocker(ctx sdk.Context, k Keeper) {
 
 func applyInflation(state *InflationState, totalTokenSupply sdk.Coins, currentTime time.Time) sdk.Coins {
 	lastAccrual := state.LastAppliedTime
-	state.LastAppliedTime = currentTime
 	mintedCoins := sdk.Coins{}
+
+	// Inflation may be set to start in the future. Do nothing in that case.
+	if currentTime.Before(lastAccrual) {
+		return mintedCoins
+	}
+
+	state.LastAppliedTime = currentTime
 
 	for i, asset := range state.InflationAssets {
 		supply := totalTokenSupply.AmountOf(asset.Denom)
