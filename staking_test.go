f25c4f6ded2ffdfbe17a1d253ba6005a1ff0768f
Henrik Aasted Sørensen <has@bitcraft.dk>
2019-10-24 11:06:56 +0200

    Add verification of penalty payout

--- staking_test.go
+++ staking_test.go
@@ -12,8 +12,17 @@ import (
 )
 
 var _ = Describe("Staking", func() {
+	const QueryNgmRewards = "total.#(denom==\"x3ngm\")"
+
 	Describe("Authority manages issuers", func() {
 		Context("", func() {
+			emcli := nt.NewEmcli(testnet.Keystore)
+
+			var (
+				Validator0Key = testnet.Keystore.Validators[0]
+				Validator2Key = testnet.Keystore.Validators[2]
+			)
+
 			It("creates a new testnet", createNewTestnet)
 
 			It("kill validator 2 and get jailed", func() {
@@ -24,6 +33,10 @@ var _ = Describe("Staking", func() {
 
 				// Allow for a few blocks
 				time.Sleep(5 * time.Second)
+
+				rewardsJson, err := emcli.QueryRewards(Validator0Key.GetAddress())
+				Expect(err).ToNot(HaveOccurred())
+				Expect(rewardsJson.Get(QueryNgmRewards).Raw).To(BeEmpty())
 
 				slash, err := listener.AwaitSlash()
 				Expect(err).ToNot(HaveOccurred())
@@ -37,7 +50,16 @@ var _ = Describe("Staking", func() {
 				Expect(slash()).ToNot(BeNil())
 				Expect(payoutEvent()).To(BeTrue())
 
-				time.Sleep(5 * time.Second)
+				time.Sleep(30 * time.Second)
+
+				rewardsJson, err = emcli.QueryRewards(Validator0Key.GetAddress())
+				Expect(err).ToNot(HaveOccurred())
+				Expect(rewardsJson.Get(QueryNgmRewards).Raw).ToNot(BeEmpty())
+
+				// Ensure that the jailed validator
+				rewardsJson, err = emcli.QueryRewards(Validator2Key.GetAddress())
+				Expect(err).ToNot(HaveOccurred())
+				Expect(rewardsJson.Get(QueryNgmRewards).Raw).To(BeEmpty())
 			})
 		})
 	})
