cd9f90638e0ad9304b3666a7dbb0487a17327ef6
Henrik Aasted Sørensen <has@bitcraft.dk>
2019-11-14 12:42:21 +0100

    Change timeout_commit to 500 ms to allow for some latency

--- cmd/emd/main.go
+++ cmd/emd/main.go
@@ -20,7 +20,7 @@ import (
 var configureConsensus = func() {
 	viper.Set("consensus.create_empty_blocks_interval", "60s")
 	viper.Set("consensus.create_empty_blocks", false)
-	viper.Set("consensus.timeout_commit", "250ms")
+	viper.Set("consensus.timeout_commit", "500ms")
 	viper.Set("consensus.timeout_propose", "2s")
 	viper.Set("consensus.peer_gossip_sleep_duration", "25ms")
 }
