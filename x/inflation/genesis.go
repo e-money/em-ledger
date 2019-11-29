742f0247fffe614310814866fc26f5cc17eb5ad9
Henrik Aasted Sørensen <has@bitcraft.dk>
2019-11-29 12:25:47 +0100

    Add License notices

--- x/inflation/genesis.go
+++ x/inflation/genesis.go
@@ -1,3 +1,7 @@
+// This software is Copyright (c) 2019 e-Money A/S. It is not offered under an open source license.
+//
+// Please contact partners@e-money.com for licensing related questions.
+
 package inflation
 
 import (
