8bb263a4638dd356f288fb84f96ed1353b107c74
Mario Karagiorgas <salem8@gmail.com>
2021-07-12 20:01:30 +0300

    Add hermes adapted gaiad dockerfile

---
+++ networks/docker/gaia/wrapper.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env sh
+
+##
+## Input parameters
+##
+ID=${ID:-0}
+LOG=${LOG:-gaiad.log}
+LOGLEVEL=info
+
+export GAIA_ID="gaia"
+export GAIADHOME="/gaiad/node${ID}"
+export GAIA_DATA="${GAIADHOME}/data"
+export GAIA_RPC_PORT=26557
+export GAIA_GRPC_PORT=9091
+export GAIA_SAMOLEANS=100000000000
+export GAIA_P2P_PORT=26556
+export GAIA_PROF_PORT=6061
+
+mkdir -p "${GAIA_DATA}"
+
+/bin/one-chain /bin/gaiad $GAIA_ID "$GAIA_DATA" $GAIA_RPC_PORT $GAIA_P2P_PORT $GAIA_PROF_PORT $GAIA_GRPC_PORT $GAIA_SAMOLEANS
