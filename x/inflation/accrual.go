77e5baccbc849fd1cd6d9e0d8b38f38dc52cfac4
Mario Karagiorgas <salem8@gmail.com>
2021-09-20 12:35:09 +0300

    Group & sort imports

--- x/inflation/accrual.go
+++ x/inflation/accrual.go
@@ -7,11 +7,9 @@ package inflation
 import (
 	"time"
 
-	"github.com/e-money/em-ledger/x/inflation/types"
-
-	"github.com/e-money/em-ledger/util"
-
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/e-money/em-ledger/util"
+	"github.com/e-money/em-ledger/x/inflation/types"
 )
 
 const (
