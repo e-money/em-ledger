e63f2086e611c52b429546edc708cf576d379ebe
Mario Karagiorgas <salem8@gmail.com>
2021-10-27 16:56:49 +0300

    Optimize Upgrade bdd test

--- networktest/testnet.go
+++ networktest/testnet.go
@@ -282,7 +282,7 @@ func IncChain(delta int64) (int64, error) {
 		// max seconds allowance for reaching the desired height
 		// increased again to accommodate an observed backup operation during the upgrade
 		// `time taken to complete the backup: 442.204684ms12:00PM INF starting ABCI with Tendermint`
-		time.Duration(delta)*24*time.Second,
+		time.Duration(delta)*16*time.Second,
 	)
 }
 
