0551366db3e55fdffe434a0faa9f88371d85ae1a
Henrik Aasted Sørensen <has@bitcraft.dk>
2022-11-23 10:49:11 +0100

    Remove header

--- x/market/types/types_test.go
+++ x/market/types/types_test.go
@@ -1,7 +1,3 @@
-// This software is Copyright (c) 2019-2020 e-Money A/S. It is not offered under an open source license.
-//
-// Please contact partners@e-money.com for licensing related questions.
-
 package types
 
 import (
